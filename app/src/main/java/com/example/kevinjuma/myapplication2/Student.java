b1e3d1f4a75cf0e09087bdc7beedd0b4d00e3e97
esiea <esiea@MacBook-de-esiea.local>
2016-12-21 02:30:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kevinjuma/myapplication2/Student.java
@@ -0,0 +1,24 @@
+package com.example.kevinjuma.myapplication2;
+
+
+public class Student {
+    private int id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+}
